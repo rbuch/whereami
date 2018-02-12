baf6de83721fe04c7baca2b1182d23212f83baf7
Ronak Buch <ronakbuch@gmail.com>
2018-02-12 15:44:32 -0600

    Fix cpuid to show ids where threads are running instead of processes

--- whereami.C
+++ whereami.C
@@ -1,31 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <limits.h>
 #include <string.h>
 #include "whereami.decl.h"
-
-int get_cpu_id()
-{
-    /* Get the the current process' stat file from the proc filesystem */
-    FILE* procfile = fopen("/proc/self/stat", "r");
-    long to_read = 8192;
-    char buffer[to_read];
-    int read = fread(buffer, sizeof(char), to_read, procfile);
-    fclose(procfile);
-
-    // Field with index 38 (zero-based counting) is the one we want
-    char* line = strtok(buffer, " ");
-    for (int i = 1; i < 38; i++)
-    {
-        line = strtok(NULL, " ");
-    }
-
-    line = strtok(NULL, " ");
-    int cpu_id = atoi(line);
-    return cpu_id;
-}
-
 
 /*mainchare*/
 class Main : public CBase_Main
@@ -74,7 +50,7 @@ public:
   {
     char hostname[HOST_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
-    mainProxy.receiveCPU(thisIndex, get_cpu_id(), std::string(hostname));
+    mainProxy.receiveCPU(thisIndex, sched_getcpu(), std::string(hostname));
   }
 
   Where(CkMigrateMessage *m) {}
